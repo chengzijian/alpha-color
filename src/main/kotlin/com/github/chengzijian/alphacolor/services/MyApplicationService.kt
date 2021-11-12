386ad9c1c92fa5f7dedf54543e45571903daa030
GitHub Action <action@github.com>
2021-11-12 07:14:49 +0000

    Template cleanup

--- src/main/kotlin/com/github/chengzijian/alphacolor/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/chengzijian/alphacolor/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.chengzijian.alphacolor.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.chengzijian.alphacolor.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/chengzijian/alphacolor/services/MyProjectService.kt
+++ src/main/kotlin/com/github/chengzijian/alphacolor/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.chengzijian.alphacolor.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.chengzijian.alphacolor.MyBundle
 
 class MyProjectService(project: Project) {
 
